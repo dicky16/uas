55081897e1bc60a199065d379e68a010f24ad3ff
djavu <djavu@172.30.47.174>
2019-12-06 13:29:48 +0700

    desain-v4

---
+++ src/tiket/Data.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author djavu
+ */
+public class Data {
+    public static ArrayList<String> jenisWisata = new ArrayList<>();
+     public static ArrayList<Integer> ArrSubTotal = new ArrayList<>();
+      public static ArrayList<Integer> ArrJumlahTiket = new ArrayList<>();
+       public static ArrayList<String> ArrTanggal = new ArrayList<>();
+    
+}
---
+++ src/tiket/Fungsi.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiket;
+
+/**
+ *
+ * @author djavu
+ */
+public class Fungsi {
+    
+   public static int getSubTotal(int a,int b){
+        int total = a*b;
+        return total;
+    }
+   public static int getJumlah(int a,int b){
+       int total = a+b;
+       return total;
+   }
+   public static int getTotal(int a,int b,int c){
+       int total = a+b+c;
+       return total;
+   } 
+}
+
+
